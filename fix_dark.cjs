c4e8ed3b24fe3e879a7ae9737a947fad29e95ddf
juzdog <info.juzdog@gmail.com>
2026-07-23 01:46:47 +0530

    ui

--- fix_dark.cjs
+++ fix_dark.cjs
@@ -3,6 +3,6 @@ const path = 'd:/Our Projects/Namma Orru foods/frontend/src/components/admin/Cou
 let content = fs.readFileSync(path, 'utf8');
 // Replace all instances of dark:something
 content = content.replace(/\bdark:[a-zA-Z0-9\/-]+\b/g, '');
-// Cleangfdg up any double spaces left behind
+// Cleangfdg up tfytany double spaces left behind
 content = content.replace(/  +/g, ' ');
 fs.writeFileSync(path, content);
