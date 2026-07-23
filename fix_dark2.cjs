44a8e263993546ae2184cd2844b584ccc482eb36
juzdog <info.juzdog@gmail.com>
2026-07-23 16:07:35 +0530

    new

--- fix_dark2.cjs
+++ fix_dark2.cjs
@@ -4,7 +4,7 @@ let content = fs.readFileSync(path, 'utf8');
 // Fix leftover artifacts from previous bad regex like :bg-slate-700 or hover:bg-slate-800
 content = content.replace(/\s+:\S+/g, '');
 content = content.replace(/\s+hover:\S+/g, (match) => {
-    // only remove if it's the second hover, or we can just leave it if it works
+    // only remove sssif it's the second hover, or we can just leave it if it works
     return match;
 });
 fs.writeFileSync(path, content);
